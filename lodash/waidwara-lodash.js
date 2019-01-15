403496c264a210f58c985107a58d03feae8b0ec4
Waidwara <287823622@qq.com>
2019-01-15 20:42:25 +0800

    first

--- lodash/waidwara-lodash.js
+++ lodash/waidwara-lodash.js
@@ -63,7 +63,7 @@ var waidwara = {
     for(var i=0;i<array.length-1;i++){
       result.push(array[i])
     }
-    return array
+    return result
   },
   indexOf:function(array,value,fromIndex=0){
     for(var i=fromIndex;i<array.length;i++){
@@ -84,6 +84,9 @@ var waidwara = {
     return array[array.length-1]
   },
   lastIndexOf:function(array,value,fromIndex=array.length-1){
+    if(fromIndex<0){
+      return -1
+    }
     for(var i=fromIndex;i>=0;i--){
       if(array[i]==value){
         return i
@@ -92,10 +95,20 @@ var waidwara = {
   },
   nth:function(array,n=0){
     if(n>=0){
-      return array[i]
+      if(n>array.length-1){
+        return -1
+      }else{
+        return array[i]
+      }  
     }
     else{
-      return array[array.length-n]
+      if(array.length+n<0){
+        return -1
+      }
+      else{
+        return array[array.length+n]
+      }
+      
     }
   },
   pull:function(array,...values){
@@ -108,5 +121,128 @@ var waidwara = {
       }  
     }
     return result
-  }
+  },
+  reverse:function(array){
+    var result=[]
+    for(var i=array.length;i>=0;i--){
+      result.push(array[i])
+    }
+    return result
+  },
+  sortedIndex:function(array,value){
+    if(array[0]>value){
+      return 0
+    }
+    else if(array[array.length-1]<value){
+      return array.length
+    }
+    eles{
+      for(var i=0;i<array.length;i++){
+        if(array[i]<=value&&value<array[i+1]){
+          return i+1
+        }
+      }
+    }
+    
+  },
+  sortedIndexOf:function(array,value){
+    for(var i=0;i<array.length;i++){
+      if(array[i]==value){
+        return i
+      }
+    }
+  },
+  sortedLastIndex:function(array,value){
+    for(var i=array.length;i>=0;i--){
+      if(array[i]==value){
+        return i
+      }
+    }
+  },
+  tail:function(array){
+    var result=[]
+    for(var i=1;i<array.length;i++){
+      result.push(array[i])
+    }
+    return result
+  },
+  take:function(array,n=1){
+    var result=[]
+    if(n>=array.length){
+      return array
+    }
+    else{
+      for(var i=0;i<n;i++){
+      result.push(array[i])
+      }
+      return result
+    }  
+  },
+  takeRight:function(array,n=1){
+    var result=[]
+    if(array.length-1-n<0){
+      return array
+    }
+    else{
+      for(var i=array.length-1;i>array.length-1-n;i--){
+      result.push(array[i])
+    }
+    return result
+    } 
+  },
+  add:function(augend,addend){
+    return augend+addend
+  },
+  ceil:function(number,precision=0){
+    
+  },
+  divide:function(dividend,divisor){
+    return dividend/divisor
+  },
+  max:function(array){
+    if(!array){
+      return undefined
+    }
+    else{
+      for(var i=1;i<array.length;i++){
+        if(array[0]<array[i]){
+          array[0]=array[i]
+        }
+      }
+      return array[0]
+    }
+  },
+  mean:function(array){
+    var sum=0
+    for(var i=0;i<array.length;i++){
+      sum=sum+array[i]
+    }
+    return sum/array.length
+  },
+  min:function(array){
+    if(!array){
+      return undefined
+    }
+    else{
+      for(var i=1;i<array.length;i++){
+        if(array[0]>array[i]){
+          array[0]=array[i]
+        }
+      }
+      return array[0]
+    }
+  },
+  multiply:function(multiplier,multiplicand){
+    return multiplier*multiplicand
+  },
+  subtract:function(minuend, subtrahend){
+    return minuend-subtrahend
+  },
+  sum:function(array){
+    var sum=0
+    for(var i=0;i<array.length;i++){
+      sum=sum+array[i]
+    }
+    return sum
+  },
 }
