90577784f807ac17a16b9c8c57562a8fc6e17e7e
Waidwara <287823622@qq.com>
2019-01-15 16:17:08 +0800

    lodash

--- lodash/waidwara-lodash.js
+++ lodash/waidwara-lodash.js
@@ -15,7 +15,7 @@ var waidwara = {
     }
     return result
   },
-  compact: function(Addary,ary){
+  comcat: function(Addary,ary){
     var result=[]
     for(var i=0;i<Addary,length;i++){
       result.push(Addary[i])
@@ -28,14 +28,10 @@ var waidwara = {
   difference:function(array,ary){
     var result=[]
     for(var i=0;i<array.length;i++){
-      for(var j=0;j<ary.length;j++){
-        if(array[i]!=ary[j]){
-          result.push(array[i])
-        }
-      }
-    }
+      
     return result
-  },
+  }
+},
   drop:function(array,size=1){
     var result=[]
     for(var i=size;i<array.length;i++){
@@ -47,6 +43,69 @@ var waidwara = {
     var result=[]
     for(var i=0;i<array.length-size;i++){
       result.push(array[i])
+    }
+    return result
+  },
+  fill:function(array,value,start=0,end=array.length){
+    for(var i=start;i<end;i++){
+      array[i]=value
+    }
+    return array
+  },
+  head:function(array){
+    if(!array){
+      return undefined
+    }
+    return array[0]
+  },
+  initial:function(array){
+    var result=[]
+    for(var i=0;i<array.length-1;i++){
+      result.push(array[i])
+    }
+    return array
+  },
+  indexOf:function(array,value,fromIndex=0){
+    for(var i=fromIndex;i<array.length;i++){
+      if(array[i]==value){
+        return i
+      }
+    }
+  },
+  join:function(array,separator=","){
+    var result=''
+    for(var i=0;i<array.length-1;i++){
+      result=result+array[i]+separator
+    }
+    result=result+array[array.length-1]
+    return result
+  },
+  last:function(array){
+    return array[array.length-1]
+  },
+  lastIndexOf:function(array,value,fromIndex=array.length-1){
+    for(var i=fromIndex;i>=0;i--){
+      if(array[i]==value){
+        return i
+      }
+    }
+  },
+  nth:function(array,n=0){
+    if(n>=0){
+      return array[i]
+    }
+    else{
+      return array[array.length-n]
+    }
+  },
+  pull:function(array,...values){
+    var result=[]
+    for(var i=0;i<array.length;i++){
+      for(var j=0;j<values.length;j++){
+        if(array[i]!=values[j]){
+        result.push(array[i])
+        }
+      }  
     }
     return result
   }
